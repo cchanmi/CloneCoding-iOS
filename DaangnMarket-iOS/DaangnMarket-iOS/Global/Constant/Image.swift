0319415f9ad20bc184f20573f480f5bbad6c949a
cchanmi <cchanmi518@gmail.com>
2022-08-06 18:20:19 +0900

    에셋 추가

---
+++ DaangnMarket-iOS/DaangnMarket-iOS/Global/Constant/Image.swift
@@ -0,0 +1,42 @@
+//
+//  Constant.swift
+//  DaangnMarket-iOS
+//
+//  Created by 황찬미 on 2022/08/06.
+//
+
+import UIKit
+
+extension Constant {
+    struct Image {
+        // icn
+        static let icChatSelected = UIImage(named: "isChatSelected")!
+        static let icChatUnselected = UIImage(named: "icChatUnselected")!
+        static let icCheckSelected = UIImage(systemName: "icCheckSelected")!
+        static let icCheckUnselected = UIImage(systemName: "icCheckUnselected")!
+        static let icHomeSelected = UIImage(systemName: "icHomeSelected")!
+        static let icHomeUnselected = UIImage(systemName: "icHomeUnselected")!
+        static let icMyDaangnSelected = UIImage(systemName: "icMyDaangnSelected")!
+        static let icMyDaangnUnselected = UIImage(systemName: "icMyDaangnUnselected")!
+        static let icMyPlaceSelected = UIImage(systemName: "icMyPlaceSelected")!
+        static let icMyPlaceUnselected = UIImage(systemName: "icMyPlaceUnselected")!
+        static let icNeighbohoodSelected = UIImage(systemName: "icNeighbohoodSelected")!
+        static let icNeighbohoodUnselected = UIImage(systemName: "icNeighbohoodUnselected")!
+        static let icBell = UIImage(systemName: "icBell")!
+        static let icFace = UIImage(systemName: "icFace")!
+        static let icFillter = UIImage(systemName: "icFillter")!
+        static let icHeartOn = UIImage(systemName: "icHeartOn")!
+        static let icHeartOff = UIImage(systemName: "icHeartOff")!
+        static let icMenu = UIImage(systemName: "icMenu")!
+        static let icMore = UIImage(systemName: "icMore")!
+        static let icOften = UIImage(systemName: "icOften")!
+        static let icSearch = UIImage(systemName: "icSearch")!
+        
+        //image
+        static let image1 = UIImage(systemName: "image1")!
+        static let image2 = UIImage(systemName: "image2")!
+        static let image3 = UIImage(systemName: "image3")!
+        static let image4 = UIImage(systemName: "image4")!
+        static let image5 = UIImage(systemName: "image5")!
+    }
+}
---
+++ DaangnMarket-iOS/DaangnMarket-iOS/Global/Extension/UIColor+.swift
@@ -0,0 +1,12 @@
+//
+//  Color.swift
+//  DaangnMarket-iOS
+//
+//  Created by 황찬미 on 2022/08/06.
+//
+
+import UIKit
+
+extension UIColor {
+    static let daangnOrange = UIColor(named: "daangnOrange")
+}
